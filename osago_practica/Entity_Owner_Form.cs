328dd61c768af6bc7be244d994bc13a6993ef472
P3-16 <p3-16@STUD.LOCAL>
2020-02-21 11:14:51 +0300

    hello again

---
+++ osago_practica/Entity_Owner_Form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace osago_practica
+{
+    public partial class Entity_Owner_Form : Form
+    {
+        public Entity_Owner_Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- osago_practica/Form1.cs
+++ osago_practica/Form1.cs
@@ -21,6 +21,21 @@ namespace osago_practica
         {
             string a;
             a = this.Text;
-        }  
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
